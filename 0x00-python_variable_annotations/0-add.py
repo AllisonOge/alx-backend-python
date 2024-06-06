70635dc2f8abf28d70bf1710a7122e76f1c3b932
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-06 19:54:30 +0000

    doc requirement

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,14 @@ Module for typed-annotated function `add`
 
 
 def add(a: float, b: float) -> float:
+    """
+    add two floating point numbers
+
+    params:
+      a: int
+      b: int
+
+    returns:
+      a + b: float
+    """
     return a + b
