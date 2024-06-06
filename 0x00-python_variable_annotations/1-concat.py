f36e066d04e91fc41510d0b8daafb671fe319dcd
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-06 20:04:01 +0000

    task#1: basic annotations-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for type-annotated function `concat`
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenate two strings
+
+    params:
+      str1: str
+      str2: str
+
+    returns:
+      str1 + str2: str
+    """
+    return str1 + str2
