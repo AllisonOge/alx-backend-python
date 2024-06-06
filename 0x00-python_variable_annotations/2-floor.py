8693a4fa8010e7945043bbf3ffd252ad8fd143c3
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-06 20:57:13 +0000

    task#2: basic annotations-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for type-annotated function `floor`
+"""
+
+
+def floor(n: float) -> int:
+    """
+    floor a float
+
+    params:
+      n: float
+
+    returns
+      int
+    """
+    return int(n)
