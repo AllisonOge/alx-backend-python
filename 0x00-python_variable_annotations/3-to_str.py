9a4ad59a1c25dcbeb09c5fbb2c86d5605b9a9c78
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-06 20:09:33 +0000

    task#3: basic annotation-to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for type-annotated function `to_str`
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    convert floating point number to string representation
+
+    params:
+      n: float
+
+    """
+    return str(n)
