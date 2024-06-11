8150d88a4c83230770e84e2c7e3657e898c37250
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-11 01:38:57 +0000

    pycodestyle requirement

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
 Module that defines an async routine, `wait_n` that takes 2 integer
-arguments (n, max_delay) and spawns wait_random n times with the 
+arguments (n, max_delay) and spawns wait_random n times with the
 specified max_delay
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
