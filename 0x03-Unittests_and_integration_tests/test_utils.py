3ed037ae2a7de513ba77c0e68a81857139ae82ce
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-01 20:42:32 +0000

    task#3: parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from parameterized import parameterized
 from utils import (
         access_nested_map,
         get_json,
+        memoize,
 )
 
 
@@ -49,3 +50,23 @@ class TestGetJson(unittest.TestCase):
         """
         mock_get.return_value.json.return_value = test_payload
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        """
+        test the `utils.memoize` decorator
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) \
+                as mock_a_method:
+            myobj = TestClass()
+            self.assertEqual(myobj.a_property, 42)  # first call
+            self.assertEqual(myobj.a_property, 42)  # second call
+            mock_a_method.assert_called_once()
