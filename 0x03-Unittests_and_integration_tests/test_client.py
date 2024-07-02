ed6faf67c855ed161969ce32a338c70bdfd5a3ea
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-02 03:29:01 +0000

    task#6: more patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -41,3 +41,26 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client._public_repos_url
         self.assertEqual(result,
                          'https://api.github.com/orgs/test_org/repos')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """
+        test that the list of repos is what you'd expect from
+        the chosen payload
+        """
+        mock_get_json.return_value = [
+            {"name": "repo1", "license": {"key": "mit"}},
+            {"name": "repo2", "license": {"key": "apache-2.0"}},
+            {"name": "repo3", "license": {"key": "mit"}}
+        ]
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = \
+                    'https://api.github.com/orgs/test_org/repos'
+            client = GithubOrgClient('test_org')
+
+            result = client.public_repos()
+            self.assertEqual(result, ['repo1', 'repo2', 'repo3'])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                    'https://api.github.com/orgs/test_org/repos')
